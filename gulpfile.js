518d43b28fa6d55a7135ac82f07107af19be01b1
joaocampinhos <joao.campinhos@gmail.com>
2014-06-11 14:55:42 +0100

    empezamos

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var gulp = require('gulp');
+
+// load plugins
+var $ = require('gulp-load-plugins')();
+
+gulp.task('styles', function () {
+  return gulp.src('app/css/main.scss')
+    .pipe($.sass({
+      style: 'nested'
+    }))
+    .pipe($.autoprefixer('last 1 version'))
+    .pipe(gulp.dest('.tmp/css'))
+    .pipe($.size());
+});
+
+gulp.task('scripts', function () {
+  return gulp.src('app/js/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter(require('jshint-stylish')))
+    .pipe($.size());
+});
+
+gulp.task('html', ['styles', 'scripts'], function () {
+  var jsFilter = $.filter('**/*.js');
+  var cssFilter = $.filter('**/*.css');
+
+  return gulp.src('app/*.html')
+    .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
+    .pipe(jsFilter)
+    .pipe($.uglify({
+      mangle: false
+    }))
+    .pipe(jsFilter.restore())
+    .pipe(cssFilter)
+    .pipe($.csso())
+    .pipe(cssFilter.restore())
+    .pipe($.useref.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest('dist'))
+    .pipe($.size());
+});
+
+gulp.task('clean', function () {
+  return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
+});
+
+gulp.task('build', ['html']);
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
+
+gulp.task('connect', function () {
+  var connect = require('connect');
+  var app = connect()
+    .use(require('connect-livereload')({ port: 35729 }))
+    .use(connect.static('app'))
+    .use(connect.static('.tmp'))
+    .use(connect.directory('app'));
+
+  require('http').createServer(app)
+    .listen(9069)
+    .on('listening', function () {
+        console.log('Started connect web server on http://localhost:9069');
+      });
+});
+
+gulp.task('serve', ['connect'], function () {
+  require('opn')('http://localhost:9068');
+});
+
+gulp.task('watch', ['connect', 'serve'], function () {
+  var server = $.livereload();
+
+  gulp.watch([
+    'app/*.html',
+    '.tmp/css/**/*.css',
+    'app/js/**/*.js',
+  ]).on('change', function (file) {
+    server.changed(file.path);
+  });
+
+  gulp.watch('app/css/**/*.scss', ['styles']);
+  gulp.watch('app/js/**/*.js', ['scripts']);
+});
