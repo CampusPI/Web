28a5f3b57760d7284bb0da8b7122938f3d4095af
João Campinhos <joao.campinhos@gmail.com>
2014-06-17 20:44:33 +0100

    done testing

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('tvApp', ['ngResource', 'btford.modal', 'ngRoute'])
+angular.module('tvApp', ['ngResource', 'btford.modal', 'ngRoute', 'googleOauth'])
 
   .config(function($routeProvider) {
     $routeProvider
@@ -15,4 +15,13 @@ angular.module('tvApp', ['ngResource', 'btford.modal', 'ngRoute'])
         templateUrl : 'templates/video.html',
         controller  : 'videoCtrl'
       });
+  }).
+
+  config(function(TokenProvider) {
+    TokenProvider.extendConfig({
+      clientId: '859779641004-cvl4ppt05dj26eprlv0eer3hrbmkpudf.apps.googleusercontent.com',
+      redirectUri: 'http://localhost:9068/bower_components/angular-oauth/src/oauth2callback.html',
+      //redirectUri: baseUrl + 'src/oauth2callback.html',  // allow lunching demo from a mirror
+      scopes: ['https://www.googleapis.com/auth/userinfo.email']
+    });
   });
--- app/js/controllers/login.js
+++ app/js/controllers/login.js
@@ -1,9 +1,37 @@
 'use strict';
 
-angular.module('tvApp').controller('loginCtrl', function ($scope,loginModal) {
+angular.module('tvApp').controller('loginCtrl', function ($scope, loginModal, $http, Token, $rootScope, $location) {
   $scope.name = 'Campeão';
   $scope.showModal = function() {
     console.log('chamou');
     loginModal.activate();
   };
+
+  $scope.$on('$routeChangeStart', function () {
+    verify();
+  });
+
+  var verify = function() {
+    console.log(Token);
+    $scope.accessToken = Token.get();
+    if ($scope.accessToken) {
+      Token.verifyAsync($scope.accessToken).
+        then(function() {
+          $scope.accessTokenVerified = true;
+          $http.get('http://localhost:8080/api/web/user', {headers: {'Authorization': 'Bearer '+$scope.accessToken}}).success(function(data) {
+            $scope.user = data;
+            console.log(data);
+          });
+        }, function() {
+          console.log('Failed to verify token.');
+        });
+    }
+  };
+
+  $scope.login = function() {
+    Token.extendConfig({state: $location.absUrl()});
+    var extraParams = {};
+    Token.getTokenWithRedirect(extraParams);
+  };
+
 });
