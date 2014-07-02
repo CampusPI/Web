3c2c0e706262b5bb7696bf99616a97c562deb259
jpgpereira <jpgpereira.92@gmail.com>
2014-07-02 13:52:14 +0100

    lista conteudos

--- app/js/controllers/type.js
+++ app/js/controllers/type.js
@@ -1,5 +1,21 @@
 'use strict';
 
-angular.module('tvApp').controller('typeContentCtrl', function ($scope, $routeParams) {
+angular.module('tvApp').controller('typeContentCtrl', function ($scope, $routeParams, VideosService, NewsService, EventsService) {
   $scope.type = $routeParams.type;
+  if($routeParams.type === 'videos'){
+    VideosService.get().then(function(data) {
+      $scope.videos = data;
+    });
+  }
+  else if($routeParams.type === 'noticias'){
+    NewsService.get().then(function(data) {
+      $scope.news = data;
+      $scope.imgDef= 'http://www.fct.unl.pt/sites/default/files/imagens/noticias/noticias.jpg';
+    });
+  }
+  else if($routeParams.type === 'eventos'){
+    EventsService.get().then(function(data) {
+      $scope.events = data;
+    });
+  }
 });
