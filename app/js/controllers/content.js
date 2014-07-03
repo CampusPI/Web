5766995c1f1f96449606736f1f9b110643f4f326
joaocampinhos <joao.campinhos@gmail.com>
2014-07-03 10:38:51 +0100

    favs yo

--- app/js/controllers/content.js
+++ app/js/controllers/content.js
@@ -1,10 +1,28 @@
 'use strict';
 
-angular.module('tvApp').controller('contentCtrl', function ($scope, $routeParams, ScheduleService) {
+angular.module('tvApp').controller('contentCtrl', function ($scope, $routeParams, ScheduleService, FavoritesService) {
   console.log($routeParams);
+  $scope.isFav = null;
   $scope.id = $routeParams.id;
   ScheduleService.getc($routeParams.id).then(function(data) {
     console.log(data);
     $scope.data = data;
   });
+
+  var isfav = function () {
+    FavoritesService.isFav($scope.id).then(function(res){
+      if (res.length > 0) {
+        $scope.isFav = true;
+      }
+      else {
+        $scope.isFav = false;
+      }
+      console.log($scope.isFav);
+    });
+  };
+
+  isfav();
+
+  $scope.rem = function(id) { FavoritesService.removeFavorite(id).then(function() { isfav(); }); };
+  $scope.add = function(id) { FavoritesService.addFavorite(id).then(function() { isfav(); }); };
 });
